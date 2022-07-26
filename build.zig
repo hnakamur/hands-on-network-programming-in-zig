abfd2792780d2bd2e6f79ada9ef563d5094c27e3
Hiroaki Nakamura <hnakamur@gmail.com>
2022-07-26 23:12:27 +0900

    Add BuildRunStepInfo in build.zig

--- build.zig
+++ build.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 const builtin = @import("builtin");
 const deps = @import("./deps.zig");
 
+const BuildRunStepInfo = struct {
+    exe_name: []const u8,
+    win_src: []const u8,
+    linux_src: []const u8,
+    run_step_name: []const u8,
+    run_step_description: []const u8,
+};
+
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -14,10 +22,13 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     {
-        const exe = b.addExecutable("chap01", if (builtin.os.tag == .windows)
-            "chap01/main_windows.zig"
-        else
-            "chap01/main_linux.zig");
+        const exe = b.addExecutable(
+            "chap01",
+            if (builtin.os.tag == .windows)
+                "chap01/main_windows.zig"
+            else
+                "chap01/main_linux.zig",
+        );
         exe.setTarget(target);
         exe.setBuildMode(mode);
         if (builtin.os.tag != .windows) {
@@ -36,11 +47,23 @@ pub fn build(b: *std.build.Builder) void {
         run_step.dependOn(&run_cmd.step);
     }
 
-    {
-        const exe = b.addExecutable("chap02_time_server", if (builtin.os.tag == .windows)
-            "chap02/time_server_windows.zig"
-        else
-            "chap02/time_server_linux.zig");
+    const infos = [_]BuildRunStepInfo{
+        .{
+            .exe_name = "chap02_time_server",
+            .win_src = "chap02/time_server_windows.zig",
+            .linux_src = "chap02/time_server_linux.zig",
+            .run_step_name = "run-chap02",
+            .run_step_description = "Run the chap02 app",
+        },
+    };
+    for (infos) |info| {
+        const exe = b.addExecutable(
+            info.exe_name,
+            if (builtin.os.tag == .windows)
+                info.win_src
+            else
+                info.linux_src,
+        );
         exe.setTarget(target);
         exe.setBuildMode(mode);
         exe.linkLibC();
@@ -53,7 +76,7 @@ pub fn build(b: *std.build.Builder) void {
             run_cmd.addArgs(args);
         }
 
-        const run_step = b.step("run-chap02", "Run the chap02 app");
+        const run_step = b.step(info.run_step_name, info.run_step_description);
         run_step.dependOn(&run_cmd.step);
     }
 }
