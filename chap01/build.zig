78822fc93ebae24420efd696784c5db643eb061b
Hiroaki Nakamura <hnakamur@gmail.com>
2022-07-26 20:53:01 +0900

    Change build step for Windows and Linux

--- chap01/build.zig
+++ chap01/build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
@@ -12,11 +13,15 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    // Windows exe
-
-    const exe = b.addExecutable("chap01", "src/main.zig");
+    const exe = b.addExecutable("chap01", if (builtin.os.tag == .windows)
+        "src/main_windows.zig"
+    else
+        "src/main_linux.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    if (builtin.os.tag != .windows) {
+        exe.linkLibC();
+    }
     deps.addAllTo(exe);
     exe.install();
 
@@ -28,32 +33,4 @@ pub fn build(b: *std.build.Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Linux exe
-
-    const linux_exe = b.addExecutable("chap01-linux", "src/main_linux.zig");
-    linux_exe.setTarget(target);
-    linux_exe.setBuildMode(mode);
-    linux_exe.linkLibC();
-    deps.addAllTo(linux_exe);
-    linux_exe.install();
-
-    const linux_run_cmd = linux_exe.run();
-    linux_run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        linux_run_cmd.addArgs(args);
-    }
-
-    const linux_run_step = b.step("run-linux", "Run the app");
-    linux_run_step.dependOn(&linux_run_cmd.step);
-
-    // Test
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-    deps.addAllTo(exe_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
